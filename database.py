1991cf385a568241bcb78d42be6fbdb04104a8d5
vikas <pvikivikas5@gmail.com>
2024-05-29 10:28:10 +0530

    v2

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import mysql.connector
+
 import function as f
 
 
@@ -7,11 +7,25 @@ connector=f.connect_user()
 cursor=connector.cursor()
 
 
-#cursor=f.connect_db(cursor)
 
 
-data=f.file_data('C:/Users/pviki/programing/pythonProject/databaseproject/dbms.xlsx')
+data=f.file_data("C:/Users/pviki/programing/pythonProject/Database-Management-System-in-python/dbms.xlsx")
 print("The column names in excel sheel:")
 column=list(data.columns)
 for i in column:
     print(i)
+
+
+cursor,db_name=f.connect_db(cursor)
+t_name=""
+while True:
+  present=input("Table already present or not ,Enter yes or No:")
+  if present.lower()=='yes':
+    t_name=input("Enter the name of the table:")
+    break
+  elif present.lower()=='no':
+    cursor,t_name=f.create_table(cursor,column)
+    break
+  else:
+     print("Enter the valid option")
+f.enter_data(db_name,data,t_name)
--- function.py
+++ function.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mysql.connector
+from sqlalchemy import create_engine
 
 
 #function to connect to user in database
@@ -20,13 +21,45 @@ def connect_db(cursor):
             
                # If execution reaches here, the database exists
                print("Connected to database:", db_name)
-               return cursor
+               return cursor,db_name
           except mysql.connector.Error as err:
             print("Database not found. Please enter a valid database name.")
+     
         
+#function to create a table
+def create_table(cursor,column):
+     command="CREATE TABLE "
+     t_name=input("Enter the name of the table to be created:")
+     command+=t_name+'('
 
+     for i in range(len(column)):
+          command+=column[i]+" "+input("Enter the datatype for "+column[i]+": ")
+          if i<len(column)-1:
+               command+=','
+     command+=");"
+     print(command)
+     cursor.execute(command)
+     print("Table create sucessfully")
+     return cursor,t_name
 
 #function to read excel data
 def file_data(path):
      data=pd.read_excel(path)
      return data
+
+
+
+def get_database_connection(database_name):
+    engine = create_engine(f"mysql+mysqlconnector://test:test@127.0.0.1/{database_name}")
+    return engine
+
+# Get database connection
+def enter_data(database_name,data,t_name):
+      engine = get_database_connection(database_name)
+      try:
+        data.to_sql(name=t_name, con=engine, if_exists='append', index=False)
+        print("Data inserted successfully.")
+      except Exception as e:
+        print(f"An error occurred: {e}")
+          
+
